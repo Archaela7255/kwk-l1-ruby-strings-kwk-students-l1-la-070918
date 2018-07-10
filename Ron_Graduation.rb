87cb1bd4c34deffc2a85023508bb5c1cfc393a82
Archaela7255 <193241@students.avhsd.org>
2018-07-10 17:21:57 +0000

    automatically backed up by learn

---
+++ Ron_Graduation.rb
@@ -0,0 +1,16 @@
+puts "Enter Name"
+g = gets.strip
+puts "Dear #{g},"
+
+puts "Enter party name, date, and time"
+p = gets.strip
+d = gets.strip
+t = gets.strip
+a = gets.strip
+puts "You are cordially invited to #{p} on #{d} at #{t}. Please RSVP no later than #{a}."
+
+puts "Sincerely,"
+
+puts "Enter host name"
+h = gets.strip
+puts "#{h}"
